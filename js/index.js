603e32880154cf0f528d100fe7862367da1916eb
Mathias Bevers <mathiasbevers@outlook.com>
2022-04-26 04:44:38 +0200

    footer done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,9 @@
 import { Octokit, App } from "https://cdn.skypack.dev/octokit";
 
-//TODO: change to secret.
-const octokit = new Octokit({ auth: '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' });
+const octokit = new Octokit({ auth: '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' });
 const response = await octokit.request("Get /repos/{owner}/{repo}/commits", { owner: 'Mongar23', repo: 'Mongar23.github.io' });
 
 const author = response.data[0].commit.author;
 let date = new Date(author.date);
-date = date.toLocaleDateString();
 
-console.log("login: " + author.name + ", date: " + date);
-
-document.getElementById("author").innerHTML = author.name;
-document.getElementById("date").innerHTML = author.date;
+document.getElementById("footerText").innerHTML = "Last edited by " + author.name + " on " + date.toLocaleDateString();
