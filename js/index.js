d53c192320ebc111a9957b9128b5049d63b890c8
Mathias Bevers <mathiasbevers@outlook.com>
2022-04-26 04:13:14 +0200

    displaying correct data needs formating

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 import { Octokit, App } from "https://cdn.skypack.dev/octokit";
 
 //TODO: change to secret.
-const octokit = new Octokit({ auth: '' });
+const octokit = new Octokit({ auth: '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' });
 const response = await octokit.request("Get /repos/{owner}/{repo}/commits", { owner: 'Mongar23', repo: 'Mongar23.github.io' });
 
-console.log("login: " + response.data[0].commit.author.name + ", date: " + response.data[0].commit.author.date);
+const author = response.data[0].commit.author;
+let date = new Date(author.date);
+date = date.toLocaleDateString();
+
+console.log("login: " + author.name + ", date: " + date);
+
+document.getElementById("author").innerHTML = author.name;
+document.getElementById("date").innerHTML = author.date;
