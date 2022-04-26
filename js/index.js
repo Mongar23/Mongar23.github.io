54ee108c7560d47844bbd74ead0dcf5057ce15a1
Mathias Bevers <mathiasbevers@outlook.com>
2022-04-26 11:33:36 +0200

    home page is presentable need to add project pages

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-import { Octokit, App } from "https://cdn.skypack.dev/octokit";
+import { Octokit } from "https://cdn.skypack.dev/octokit";
 
-const octokit = new Octokit({ auth: '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' });
-const response = await octokit.request("Get /repos/{owner}/{repo}/commits", { owner: 'Mongar23', repo: 'Mongar23.github.io' });
+const octokit = new Octokit();
+const response = await octokit.request("GET /repos/{owner}/{repo}/commits", { owner: 'Mongar23', repo: 'Mongar23.github.io' });
 
 const author = response.data[0].commit.author;
 let date = new Date(author.date);
